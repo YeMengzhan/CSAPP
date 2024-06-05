696857aa34c986ea82b85f2a57eb75f628951c83
Ye Mengzhan <lebronymz@gmail.com>
2024-06-05 13:49:15 +0800

    Changes to be committed: 	modified:   HelloWorldC/hello.c

--- HelloWorldC/hello.c
+++ HelloWorldC/hello.c
@@ -1,41 +1,37 @@
 /*
- * srl-sra.c
+ * leftmost-one.c
  */
-
 #include <stdio.h>
 #include <assert.h>
 
-unsigned srl(unsigned x, int k) {
-  unsigned xsra = (int) x >> k;
-
-  int w = sizeof(int) << 3;
-  int mask = (int) 0xffffffff << (w - k);
-  return xsra & ~mask;
-}
-
-int sra(int x, int k) {
-  int xsrl = (unsigned) x >> k;
-
-  int w = sizeof(int) << 3;
-  int mask = (int) -1 << (w - k);
-  //let mask remain unchanged when the first bit of x is 1, otherwise 0.
-  int m = 1 << (w - 1);
-  mask &= ! (x & m) - 1;
-  return xsrl | mask;
+/*
+ * Generate mask indicating leftmost 1 in x. Assume w=32
+ * For example, 0xFF00 -> 0x8000, and 0x6000 -> 0x4000.
+ * If x = 0, then return 0
+ */
+int leftmost_one(unsigned x) {
+  /*
+   * first, generate a mask that all bits after leftmost one are one
+   * e.g. 0xFF00 -> 0xFFFF, and 0x6000 -> 0x7FFF
+   * If x = 0, get 0
+   */
+  x |= x >> 1;
+  x |= x >> 2;
+  x |= x >> 4;
+  x |= x >> 8;
+  x |= x >> 16;
+  /*
+   * then, do (mask >> 1) + (mask && 1), in which mask && 1 deals with case x = 0, reserve leftmost bit one
+   * that's we want
+   */
+  return (x >> 1) + (x && 1);
 }
 
 int main(int argc, char* argv[]) {
-  unsigned test_unsigned = 0x12345678;
-  int test_int = 0x12345678;
-
-  assert(srl(test_unsigned, 4) == test_unsigned >> 4);
-  assert(sra(test_int, 4) == test_int >> 4);
-
-  test_unsigned = 0x87654321;
-	test_int = 0x87654321;
-
-	assert (srl (test_unsigned, 4) == test_unsigned >> 4);
-	assert (sra (test_int, 4) == test_int >> 4);
-  
+  assert(leftmost_one(0xFF00) == 0x8000);
+  assert(leftmost_one(0x6000) == 0x4000);
+  assert(leftmost_one(0x0) == 0x0);
+  assert(leftmost_one(0x80000000) == 0x80000000);
   return 0;
 }
+
